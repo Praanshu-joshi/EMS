e622d34892b2390e33e543eceb26ec27aa5099e3
Praanshu-joshi <impj1729@gmail.com>
2025-11-20 02:11:22 +0530

    added style

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -7,15 +7,15 @@ const AllTask = () => {
 
    
   return (
-    <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
-        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-[38%]'>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded h-[30%]'>
             <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
             <h3 className='text-lg font-medium w-1/5'>New Task</h3>
             <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
             <h5 className='text-lg font-medium w-1/5'>Completed</h5>
             <h5 className='text-lg font-medium w-1/5'>Failed</h5>
         </div>
-        <div className=''>
+        <div className='h-[70%] overflow-auto hidescroll'>
         {userData.map(function(elem,idx){
             return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2 className='text-lg font-medium  w-1/5'>{elem.firstName}</h2>
